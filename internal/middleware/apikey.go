377ef9f1d5944b07d532a3061795c27a4dfc7d6c
Marceli Borowczak <marcelborowczak@outlook.com>
2024-10-19 14:36:55 +0200

    feat: api key refactor

--- internal/middleware/apikey.go
+++ internal/middleware/apikey.go
@@ -7,7 +7,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// APIKeyMiddleware - Middleware to validate API keys and user ownership
 func APIKeyMiddleware(apiKeyService services.APIKeyService) mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -17,24 +16,15 @@ func APIKeyMiddleware(apiKeyService services.APIKeyService) mux.MiddlewareFunc {
 				return
 			}
 
-			user, _ := services.UserFromContext(r.Context())
-			if user == nil {
-				http.Error(w, "User not authenticated", http.StatusUnauthorized)
-				return
-			}
-
-			apiKeyDetails, err := apiKeyService.GetAPIKeyByKey(r.Context(), apiKey)
+			user, subscription, err := apiKeyService.GetUserAndSubscriptionByAPIKey(r.Context(), apiKey)
 			if err != nil {
 				http.Error(w, "Invalid API key", http.StatusUnauthorized)
 				return
 			}
 
-			if apiKeyDetails.UserID != user.ID {
-				http.Error(w, "API key does not belong to the user", http.StatusForbidden)
-				return
-			}
-
-			next.ServeHTTP(w, r)
+			// Add the user and subscription to the request context
+			ctx := services.WithUserAndSubscriptionContext(r.Context(), user, subscription)
+			next.ServeHTTP(w, r.WithContext(ctx))
 		})
 	}
 }
